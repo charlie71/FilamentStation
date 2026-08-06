c8ba35f926d26266c76f3ba1b81159a3b05714da
EnkHTLKapfenberg <97608670+EnkHTLKapfenberg@users.noreply.github.com>
2026-08-06 13:55:08 +0200

    LVGL Mem ist moved to PSRAM

--- src/FilamentStationLvConf.h
+++ src/FilamentStationLvConf.h
@@ -6,6 +6,9 @@
 #define LV_DEF_REFR_PERIOD 33
 #define LV_DPI_DEF 130
 #define LV_MEM_SIZE (128U * 1024U)
+#define LV_MEM_POOL_INCLUDE <esp_heap_caps.h>
+#define LV_MEM_POOL_ALLOC(size) \
+  heap_caps_malloc((size), MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT)
 #define LV_FONT_MONTSERRAT_18 1
 #define LV_FONT_MONTSERRAT_20 1
 #define LV_FONT_MONTSERRAT_24 1
